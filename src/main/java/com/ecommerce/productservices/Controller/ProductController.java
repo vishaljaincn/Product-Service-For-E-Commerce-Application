3c3a798666bc03603cec2b7e0a5f542b71ad8fff
vishaljain <vishaljaincn@gmail.com>
2024-04-09 01:23:32 +0530

    Added comments for better understanding of the initial controller class

--- src/main/java/com/ecommerce/productservices/Controller/ProductController.java
+++ src/main/java/com/ecommerce/productservices/Controller/ProductController.java
@@ -6,21 +6,48 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
-
-//    https://fakestoreapi.com/products/15
-
-    @GetMapping("/{identity}/{temp}")
-    public String getProductById(@PathVariable("identity") Long id, @PathVariable("temp") String x) {
-        return "Here's your product "  + id + x;
+    //FOR GET REQUEST USE BELOW IN POSTMAN
+/*
+http://localhost:8080/products/5/macbookair?category=laptops
+ */
+    @RequestMapping("/{id}/{name}")
+    //@PathVariable extracts the required variable provided in the URI. We can use multiple Path Variables
+    //@RequestParam - Similar to path variables, user input can be provided using @RequestParam annotation.
+    public String getProductById(@PathVariable("id") Long id,
+                                 @PathVariable("name") String name,
+                                 @RequestParam("category") String category) {
+        return "Here's your product " + id + " " + name + " " + category;
     }
 
-    //    @RequestBody Product product -> Converts the received json to a Product java object
-    @PostMapping("")
-    public String createProduct(@RequestBody Product product) {
-        System.out.println(product.getCategory());
-        System.out.println(product.getName());
-        System.out.println(product.getPrice());
-        return "Product created.";
-    }
+//FOR POST REQUEST USE BELOW IN POSTMAN
+/*
 
+URL - http://localhost:8080/products/postproduct
+
+REQUEST BODY BELOW - should contain a JSON representation of the product
+{
+        "id": 5,
+        "name": "macbook",
+        "description": "helps in coding smoothly",
+        "price": 86000,
+        "imageUrl": "https://www.mobigyaan.com/wp-content/uploads/2020/03/Apple-Macbook-Air-2020-1.jpg",
+        "category": "laptop"
 }
+ */
+
+    @PostMapping("/postproduct")
+    // @RequestBody is used to deserialize the incoming payload/JSON to a Product Java object
+    // @ResponseBody is used to serialize the returned Product Java object into JSON
+    public @ResponseBody Product createProduct(@RequestBody Product product) {
+        System.out.println(product.getId());
+        System.out.println(product.getName());
+        System.out.println(product.getDescription());
+        System.out.println(product.getImageUrl());
+        System.out.println(product.getCategory());
+        //return "Product created.";
+        // Returning the created product
+        return product;
+    }
+}
+
+
--- src/main/java/com/ecommerce/productservices/Model_Entity/Product.java
+++ src/main/java/com/ecommerce/productservices/Model_Entity/Product.java
@@ -3,6 +3,9 @@ package com.ecommerce.productservices.Model_Entity;
 import lombok.Getter;
 import lombok.Setter;
 
+//@Getter and @Setter are annotation belonging to Lombok library, these can be used to automatically
+// generate getter and setter methods for fields in a Java class. This can save you a lot of time and
+// boilerplate code, especially if you have a lot of fields in your class.
 @Getter
 @Setter
 public class Product {
